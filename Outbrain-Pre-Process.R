a50473d3038df6028774019c6cc511ab2455a4d0
mrbraveheart71 <mrbraveheart71@gmail.com>
2016-12-11 21:14:51 -0500

    commit 12/11/2016

--- Outbrain-Pre-Process.R
+++ Outbrain-Pre-Process.R
@@ -9,12 +9,7 @@ documents_meta <- fread("../Data/documents_meta.csv", select = c("document_id","
 #documents_entities <- fread("../Data/documents_entities.csv")
 documents_categories <- fread("../Data/documents_categories.csv")
 documents_topics <- fread("../Data/documents_topics.csv")
-#documents_meta <- fread("../Data/documents_meta.csv")
 promoted_content <- fread("../Data/promoted_content.csv")
-
-feature.names <- c("prob","count","ad_count","ad_doc_prob","ad_doc_count","ad_source_prob","ad_source_count",
-                   "advertiser_source_prob","advertiser_source_count","ad_publisher_prob","ad_publisher_count",
-                   "ad_category_prob","ad_category_count","ad_topic_prob","ad_topic_count")
 
 if (runType=='Train') {
   display_ids <- unique(clicks_train$display_id)
@@ -62,10 +57,30 @@ rm(events)
 rm(documents_meta)
 rm(promoted_content)
 rm(max_doc_cat)
+rm(max_doc_top)
 rm(documents_topics)
+gc()
+# now do advertiser combinations
+# setkeyv(clicks_train,c("display_id","advertiser_id"))
+# advertiser_comb <- clicks_train[,.(advertiser_comb=paste(rev(advertiser_id),collapse=" ")),by=display_id]
+# advertiser_comb$advertiser_id <- clicks_train$advertiser_id[clicks_train$clicked==1]
+# setkeyv(advertiser_comb,c("advertiser_comb"))
+# advertiser_comb$advertiser_comb_int <- as.integer(as.factor(advertiser_comb$advertiser_comb))
+#rm(advertiser_comb)
+# gc()
+clicks_train[, timestampHour := hour(ISOdatetime(1970,01,01,0,0,0) + 1465876799998/1e3 + timestamp/1e3)]
+gc()
 
 click_prob = clicks_train[,.(sum(clicked)/.N)]
-ad_id_metrics <- clicks_train[clicks_train$display_id %in% train_display_ids,j=list(prob=mean(clicked),count=length(clicked)),by=list(ad_id)]
+click_prob <- as.numeric(click_prob)
+# ad_id_metrics <- clicks_train[clicks_train$display_id %in% train_display_ids,
+#                               j=list(prob=mean(clicked),count=length(clicked)),by=list(ad_id)]
+ad_id_metrics <- clicks_train[clicks_train$display_id %in% train_display_ids,
+                               j=list(prob=(sum(clicked)+click_prob*40)/(length(clicked)+40),count=length(clicked)),by=list(ad_id)]
+ad_hours_metrics <- clicks_train[clicks_train$display_id %in% train_display_ids,j=list(ad_hour_prob=mean(clicked),ad_hour_count=length(clicked)),
+                               by=list(ad_id,timestampHour)]
+# advertiser_id_metrics <- clicks_train[clicks_train$display_id %in% train_display_ids,j=list(advertiser_prob=mean(clicked),advertiser_count=length(clicked)),by=list(advertiser_id)]
+# campaign_id_metrics <- clicks_train[clicks_train$display_id %in% train_display_ids,j=list(campaign_prob=mean(clicked),campaign_count=length(clicked)),by=list(campaign_id)]
 # ad_geo_metrics <- clicks_train[clicks_train$display_id %in% train_display_ids,j=list(ad_geo_prob=mean(clicked),ad_geo_count=length(clicked)),
 #                                by=list(ad_id,geo_location)]
 ad_doc_metrics <- clicks_train[clicks_train$display_id %in% train_display_ids,j=list(ad_doc_prob=mean(clicked),ad_doc_count=length(clicked)),
@@ -86,9 +101,32 @@ ad_topic_metrics <- clicks_train[clicks_train$display_id %in% train_display_ids,
 advertiser_source_metrics <- clicks_train[clicks_train$display_id %in% train_display_ids,j=list(advertiser_source_prob=mean(clicked),advertiser_source_count=length(clicked)),
                                           by=list(advertiser_id,source_id)]
 
-save("ad_id_metrics","click_prob","ad_doc_metrics", "ad_source_metrics","advertiser_source_metrics","ad_publisher_metrics",
-     "ad_category_metrics","ad_topic_metrics","feature.names","event.col.names",file=saveFileName)
-rm("ad_id_metrics","click_prob","ad_doc_metrics","ad_source_metrics","advertiser_source_metrics","ad_publisher_metrics","ad_category_metrics",
-   "ad_topic_metrics")
-gc()
+advertiser_publisher_metrics <- clicks_train[clicks_train$display_id %in% train_display_ids,j=list(advertiser_publisher_prob=mean(clicked),advertiser_publisher_count=length(clicked)),
+                                          by=list(advertiser_id,publisher_id)]
+
+advertiser_geo_metrics <- clicks_train[clicks_train$display_id %in% train_display_ids,j=list(advertiser_geo_prob=mean(clicked),advertiser_geo_count=length(clicked)),
+                                             by=list(advertiser_id,geo_location)]
+
+feature.names <- c("prob","count","ad_count","ad_doc_prob","ad_doc_count","ad_source_prob","ad_source_count",
+                   "advertiser_source_prob","advertiser_source_count","ad_publisher_prob","ad_publisher_count",
+                   "ad_category_prob","ad_category_count","ad_topic_prob","ad_topic_count",
+                   "advertiser_publisher_prob","advertiser_publisher_count",
+                   "advertiser_geo_prob","advertiser_geo_count",
+                   "advertiser_geo_prob","advertiser_geo_count",
+                   #"timestampHour","platform",
+                   "ad_hour_prob","ad_hour_count","ad_display_prob","ad_display_count")
+
+if (runType=='Train') {
+  save("ad_id_metrics","advertiser_geo_metrics","ad_hours_metrics",
+       "click_prob","ad_doc_metrics", "ad_source_metrics","advertiser_source_metrics","ad_publisher_metrics",
+       "ad_category_metrics","ad_topic_metrics","advertiser_publisher_metrics","feature.names","event.col.names",
+       "sample_display_ids","train_display_ids","valid_display_ids",file=saveFileName)
+} else {
+  save("ad_id_metrics","advertiser_geo_metrics","ad_hours_metrics",
+       "click_prob","ad_doc_metrics", "ad_source_metrics","advertiser_source_metrics","ad_publisher_metrics",
+       "ad_category_metrics","ad_topic_metrics","advertiser_publisher_metrics","feature.names","event.col.names",
+       "train_display_ids",file=saveFileName)
+}
+# rm("ad_id_metrics","click_prob","ad_doc_metrics","advertiser_publisher_metrics","ad_source_metrics","advertiser_source_metrics","ad_publisher_metrics","ad_category_metrics",
+#    "ad_topic_metrics")
 
--- Outbrain-XGBOOST-Model.R
+++ Outbrain-XGBOOST-Model.R
@@ -2,12 +2,20 @@ library(data.table)
 library(Metrics)
 library(xgboost)
 
+setwd("C:/R-Studio/Outbrain/Outbrain R")
 runType <- "Train"
-source("Outbrain-Pre-Process.R")
+source("Outbrain-Pre-Process.R",echo=TRUE)
 
 clicks_train <- clicks_train[clicks_train$display_id %in% valid_display_ids]
 setkeyv(clicks_train,"ad_id")
 clicks_train <- merge( clicks_train, ad_id_metrics, all.x = T )
+
+
+
+display_ad_metrics <- clicks_train[,j=list(ad_display_prob=mean(prob),ad_display_count=sum(count)),
+                              by=list(display_id)]
+setkeyv(clicks_train,"display_id")
+clicks_train <- merge( clicks_train, display_ad_metrics, all.x = T )
 # setkeyv(clicks_train,c("ad_id","geo_location"))
 # clicks_train <- merge( clicks_train, ad_geo_metrics, all.x = T )
 setkeyv(clicks_train,c("ad_id","document_id"))
@@ -22,8 +30,18 @@ setkeyv(clicks_train,c("ad_id","category_id"))
 clicks_train <- merge( clicks_train, ad_category_metrics, all.x = T )
 setkeyv(clicks_train,c("ad_id","topic_id"))
 clicks_train <- merge( clicks_train, ad_topic_metrics, all.x = T )
+setkeyv(clicks_train,c("advertiser_id","publisher_id"))
+clicks_train <- merge( clicks_train, advertiser_publisher_metrics, all.x = T )
+setkeyv(clicks_train,c("advertiser_id","geo_location"))
+clicks_train <- merge( clicks_train, advertiser_geo_metrics, all.x = T )
+setkeyv(clicks_train,c("ad_id","timestampHour"))
+clicks_train <- merge( clicks_train, ad_hours_metrics, all.x = T )
+# setkeyv(clicks_train,c("advertiser_id"))
+# clicks_train <- merge( clicks_train, advertiser_id_metrics, all.x = T )
+# setkeyv(clicks_train,c("campaign_id"))
+# clicks_train <- merge( clicks_train, campaign_id_metrics, all.x = T )
 
-clicks_train$platform <- as.integer(clicks_train$platform)
+#clicks_train$platform <- as.integer(clicks_train$platform)
 
 # Get Display samples to down-scale data
 # do an xgboost
@@ -34,22 +52,23 @@ strain <- which(clicks_train$display_id %in% strainDisplayIds)
 svalid <- which(clicks_train$display_id %in% svalidDisplayIds)
 
 xgb_params = list(
-  seed = 0,colsample_bytree = 1,colsample_bylevel=1,subsample = 0.9,
+  seed = 0,colsample_bytree = 0.9,colsample_bylevel=1,subsample = 0.9,
   eta = 0.1,max_depth =10,num_parallel_tree = 1,
-  min_child_weight = 10,objective='reg:linear'
+  min_child_weight = 10,objective='binary:logistic'
   #objective="reg:linear"
 )
-
+gc()
 dtrain = xgb.DMatrix(as.matrix(clicks_train[strain,feature.names,with=FALSE]), label=clicks_train$clicked[strain], missing=NA)
 dvalid = xgb.DMatrix(as.matrix(clicks_train[svalid,feature.names,with=FALSE]), label=clicks_train$clicked[svalid], missing=NA)
 
 watchlist <- list(valid=dvalid,train=dtrain)
 xgboost.fit <- xgb.train (data=dtrain,xgb_params,missing=NA,early.stop.round = 5,
                           #eval_metric="auc",
+                          eval_metric="logloss",
                           nrounds=50,
                           maximize=FALSE,verbose=1,watchlist = watchlist)
 
-# Valid Score of 36.39
+# Valid Score of 35.78
 save("xgboost.fit",file="Outbrain XGBOOST Model")
 importance_matrix <- xgb.importance(model = xgboost.fit, feature.names)
 options(scipen=999)
@@ -58,18 +77,20 @@ print(importance_matrix,200)
 rm(dtrain,dvalid)
 gc()
 dall = xgb.DMatrix(as.matrix(clicks_train[,feature.names,with=FALSE]), label=clicks_train$clicked, missing=NA)
+clicks_train <- clicks_train[,c("ad_id","display_id","clicked"),with=FALSE]
+gc()
 clicks_train$preds <- predict(xgboost.fit,newdata=dall)
 
 # in sample testing
 #
 setkey(clicks_train,"preds")
-clicks_train <- clicks_train[,c("ad_id","display_id","clicked","preds"),with=FALSE]
 train.preds <- clicks_train[,.(ad_id=paste(rev(ad_id),collapse=" ")),by=display_id]
 train.preds <- train.preds[order(train.preds$display_id)]
 clicks_train <- clicks_train[order(clicks_train$display_id)]
 clicks_train_clicked <- clicks_train[clicks_train$clicked==1]
 
 # now just test validation Displays
+#s <- sample(svalidDisplayIds,100000)
 s <- sample(valid_display_ids,100000)
 
 predicted <- lapply(train.preds$ad_id[train.preds$display_id %in% s], function(x) as.numeric(tstrsplit(x,split=" ")))
@@ -77,7 +98,7 @@ actual <- as.list(clicks_train_clicked$ad_id[clicks_train_clicked$display_id %in
   
 map12 <- mapk(12, actual, predicted)
 print(map12)
-# Results was 67.08
+# Results was 66.5
 
 #
 # Now prepare test
@@ -86,12 +107,13 @@ library(data.table)
 library(Metrics)
 library(xgboost)
 
-load("Outbrain XGBOOST Model")
+setwd("C:/R-Studio/Outbrain/Outbrain R")
 runType <- "Test"
-source("Outbrain-Pre-Process.R")
-rm(clicks_train)
+#source("Outbrain-Pre-Process.R")
+#rm(clicks_train)
 gc()
 load('Outbrain Aggregate Tables Test')
+load("Outbrain XGBOOST Model")
 
 clicks_test   <- fread( "../Data/clicks_test.csv", select=c("ad_id","source_id","document_id","display_id"))
 events <- fread("../Data/events.csv",select = event.col.names)
@@ -123,17 +145,22 @@ setkeyv(max_doc_top,"document_id")
 setkeyv(clicks_test,"document_id")
 clicks_test <- merge(clicks_test,max_doc_top,all.x=T)
 
-rm(events)
-rm(documents_meta)
+rm("events","documents_meta","promoted_content","documents_categories","documents_topics")
+gc()
 
-#load("Outbrain Aggregate Tables")
+clicks_test[, timestampHour := hour(ISOdatetime(1970,01,01,0,0,0) + 1465876799998/1e3 + timestamp/1e3)]
 
+setkeyv(clicks_test,"ad_id")
+clicks_test  <- merge( clicks_test , ad_id_metrics, all.x = T )
 display_metrics <- clicks_test[,j=list(ad_count=length(ad_id)),by=list(display_id)]
 setkeyv(clicks_test,c("display_id"))
 clicks_test <- merge( clicks_test, display_metrics, all.x = T )
 
-setkeyv(clicks_test,"ad_id")
-clicks_test  <- merge( clicks_test , ad_id_metrics, all.x = T )
+display_ad_metrics <- clicks_test[,j=list(ad_display_prob=mean(prob),ad_display_count=sum(count)),
+                                   by=list(display_id)]
+setkeyv(clicks_test,"display_id")
+clicks_test <- merge( clicks_test, display_ad_metrics, all.x = T )
+
 # setkeyv(clicks_test,c("ad_id","geo_location"))
 # clicks_test  <- merge( clicks_test , ad_geo_metrics, all.x = T )
 setkeyv(clicks_test,c("ad_id","document_id"))
@@ -150,17 +177,74 @@ clicks_test <- merge( clicks_test, ad_category_metrics, all.x = T )
 setkeyv(clicks_test,c("ad_id","topic_id"))
 clicks_test <- merge( clicks_test, ad_topic_metrics, all.x = T )
 
+setkeyv(clicks_test,c("advertiser_id","publisher_id"))
+clicks_test <- merge( clicks_test, advertiser_publisher_metrics, all.x = T )
+setkeyv(clicks_test,c("advertiser_id","geo_location"))
+clicks_test <- merge( clicks_test, advertiser_geo_metrics, all.x = T )
+setkeyv(clicks_test,c("ad_id","timestampHour"))
+clicks_test <- merge( clicks_test, ad_hours_metrics, all.x = T )
+
 clicks_test <- clicks_test[,c(feature.names,"ad_id","display_id"),with=FALSE]
 #clicks_test$platform <- as.integer(clicks_test$platform)
-rm("ad_id_metrics","click_prob","ad_doc_metrics","ad_source_metrics","advertiser_source_metrics","ad_publisher_metrics","ad_category_metrics")
-
-dtest = xgb.DMatrix(as.matrix(clicks_test[,feature.names,with=FALSE]),missing=NA)
+rm("ad_id_metrics","click_prob","ad_doc_metrics","ad_source_metrics","advertiser_source_metrics","ad_publisher_metrics","ad_category_metrics",
+   "ad_topic_metrics")
 gc()
-clicks_test <- clicks_test[,c("ad_id","display_id"),with=FALSE]
-clicks_test$preds <- predict(xgboost.fit,newdata=dtest)
+save("clicks_test","feature.names",file="Outbrain Test File")
+
+
+# Now prepare submission
+library(data.table)
+library(Metrics)
+library(xgboost)
+
+setwd("C:/R-Studio/Outbrain/Outbrain R")
+load("Outbrain Test File")
+load("Outbrain XGBOOST Model")
+# Break it into two pieces
+half <- as.integer(nrow(clicks_test)/2)
+full <- as.integer(nrow(clicks_test))
+dtest = xgb.DMatrix(as.matrix(clicks_test[1:half,feature.names,with=FALSE]),missing=NA)
+gc()
+clicks_test[1:half, preds := predict(xgboost.fit,newdata=dtest)]
+rm(dtest)
+gc()
+dtest = xgb.DMatrix(as.matrix(clicks_test[(half+1):full,feature.names,with=FALSE]),missing=NA)
+clicks_test[(half+1):full, preds := predict(xgboost.fit,newdata=dtest)]
+rm(dtest)
+gc()
+#clicks_test <- clicks_test[,c("ad_id","display_id","preds"),with=FALSE]
+#clicks_test$preds <- predict(xgboost.fit,newdata=dtest)
 
 setkey(clicks_test,"preds")
 submission <- clicks_test[,.(ad_id=paste(rev(ad_id),collapse=" ")),by=display_id]
 setkey(submission,"display_id")
 
 write.csv(submission,file = "submission.csv",row.names = F)
+
+
+# 1:          ad_publisher_prob 0.4275698983 0.024007723 0.02561221
+# 2:     advertiser_source_prob 0.1636324116 0.027049428 0.03161242
+# 3:            ad_display_prob 0.0735522401 0.229355911 0.15736245
+# 4:             ad_source_prob 0.0528760031 0.030723863 0.02514576
+# 5:                   ad_count 0.0511424293 0.110287205 0.04808650
+# 6:                ad_doc_prob 0.0459264892 0.077174699 0.03877876
+# 7:  advertiser_publisher_prob 0.0367376890 0.032974111 0.03460193
+# 8:           ad_category_prob 0.0333939488 0.024185786 0.03324499
+# 9:                       prob 0.0253212370 0.058768867 0.06487862
+# 10:               ad_hour_prob 0.0135797341 0.030727659 0.04891339
+# 11:           ad_display_count 0.0109158259 0.064097648 0.08200997
+# 12:        advertiser_geo_prob 0.0106742642 0.045744795 0.05930245
+# 13:               ad_doc_count 0.0093811166 0.028137957 0.02559101
+# 14:                      count 0.0089232915 0.048099174 0.05599491
+# 15:              ad_topic_prob 0.0066246507 0.012415467 0.02548500
+# 16:         ad_publisher_count 0.0050570165 0.018797682 0.02296194
+# 17:            ad_source_count 0.0049571919 0.024371096 0.02705396
+# 18:          ad_category_count 0.0035046096 0.017589095 0.02936500
+# 19:    advertiser_source_count 0.0033626870 0.017716949 0.02815647
+# 20: advertiser_publisher_count 0.0029227106 0.020490242 0.02724478
+# 21:              ad_hour_count 0.0028624455 0.015696501 0.02913177
+# 22:       advertiser_geo_count 0.0024940935 0.012426703 0.03585286
+# 23:             ad_topic_count 0.0018996378 0.004779574 0.01961200
+# 24:                   platform 0.0017948398 0.021689260 0.01043146
+# 25:              timestampHour 0.0008935386 0.002692605 0.01356938
+# Feature         Gain       Cover  Frequence
